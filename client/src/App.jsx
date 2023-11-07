ae96b6d5be18d270149a79e8f908988cfbb9051c
unknown <trahy032@gmail.com>
2023-11-07 22:46:09 +1030

    imported apollo client to app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,39 @@ import {
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import Navbar from "./components/Navbar";
+
+// constructs main GQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// middleware that attacehs JWT token to each request as authentication
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// sets client to execute middleware prior to making request to GQL API
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <ApolloProvider client={client}>
+        <Navbar />
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
