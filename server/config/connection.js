5d06d828ebc20538d1058b7aa2ac7f758cf9de5a
unknown <trahy032@gmail.com>
2023-11-14 10:45:39 +1030

    updated connection js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
+  process.env.MONGODB_URI ||
+    "mongodb+srv://mdb-user1:vj1UOsxW03mhYjya@cluster0.xtw0lp0.mongodb.net/?retryWrites=true&w=majority"
 );
 
 module.exports = mongoose.connection;
